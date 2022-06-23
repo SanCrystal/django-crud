142865ef25ef39bc48d2d649d1d03e562441ba8d
sanCrystal <anyanwu.amanzearthur@gmail.com>
2022-06-23 13:44:06 +0100

    final touch

--- Santa007/Santa007/urls.py
+++ Santa007/Santa007/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from .views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',Home.as_view(),name="home"),
     path('blog/',include('blog.urls'),name="blog"),
 ]
